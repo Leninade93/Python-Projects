fd6d0d349cac64b0b4cbbe192e0f5ec912167346
nick <narvoitss@gmail.com>
2022-06-05 01:16:38 -0500

    basic syntax work with bash scripting

---
+++ Bash Projects/basics.sh
@@ -0,0 +1,23 @@
+!#/bin/bash  
+
+let a=879
+echo -n "The value of \"a\" is now: "
+echo $a
+
+let a=a+15
+echo "The value of \"a\" is now: $a"
+
+echo -n "The values of \"a\" in the loop are: "
+for a in 7 8 9 11
+do  
+    if test $a -eq 11
+    then   
+        echo "$a END"
+        break
+    elif test $a -lt 8
+    then
+        echo "BEGIN $a, "
+    else
+        echo -n "$a, "
+    fi
+done
